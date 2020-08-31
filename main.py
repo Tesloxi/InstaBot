74a486ac0f131073b5ee10ccbce1f7392cd2e315
Luka Judas <lukajudas@gmail.com>
2020-08-31 11:50:37 +0200

    Writes logs in a file

--- main.py
+++ main.py
@@ -5,15 +5,26 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as Ec
 import time
 
+try:
+    f = open("./logs.txt", "r")
+except FileNotFoundError:
+    username = input('Your username : ')
+    password = input('Your password : ')
+    f = open('./logs.txt', 'a')
+    f.write(username + '\n')
+    f.write(password)
+    f.close()
+finally:
+    f = open("./logs.txt", "r")
+    username = f.readline()
+    password = f.readline()
+    f.close()
+
 message_to = input('Enter the username of the person you want to send a message to : ')
 message = input('Enter your message : ')
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-
-username = 'luka_jd5'
-password = 'pKNkdaf%Zh#ge!!a3o42'
-
 driver.get("https://instagram.com/" + message_to)
 
 header = driver.find_elements_by_tag_name("header")
@@ -57,41 +68,7 @@ if header:
         driver.quit()
 
     except:
-        print('error')
+        print('Cannot contact this person')
 else:
     print("Can't find the username you entered")
     driver.quit()
-
-"""username_field = driver.find_element_by_name("username")
-username_field.send_keys(username)
-
-password_field = driver.find_element_by_name("password")
-password_field.send_keys(password)
-
-connexion_button = driver.find_elements_by_tag_name("button")
-connexion_button[2].click()
-
-#a = driver.find_element_by
-
-try:
-    div = WebDriverWait(driver, 10).until(
-        Ec.presence_of_element_located((By.CSS_SELECTOR, "div.cmbtv"))
-    )
-    later = div.find_element_by_tag_name("button")
-    later.click()
-
-    div = WebDriverWait(driver, 10).until(
-        Ec.presence_of_element_located((By.CSS_SELECTOR, "div.mt3GC"))
-    )
-    later = div.find_element_by_class_name("HoLwm")
-    later.click()
-
-    div = driver.find_element_by_css_selector("div.pbgfb.Di7vw ")
-    div.click()
-
-    input_field = driver.find_element_by_css_selector("input.XTCLo.x3qfX")
-    input_field.send_keys(msgto)
-    input_field.send_keys(Keys.RETURN)
-except:
-    print('error')
-    driver.quit()"""
